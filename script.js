36dfac7272ded54f7a63e503b6967d1103ce6db6
bonnerac <alex.c.bonner@gmail.com>
2019-12-18 22:01:47 -0600

    got js to loop questions and got the older question to dissapear before new question appears on screen

--- script.js
+++ script.js
@@ -10,20 +10,37 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
+    {
+      title: "Which of the following is a JavaScipt term, and not the name of a boy band?",
+      choices: ["BTS", "O-TOWN", "JSON", "NSYNC"],
+      answer: "JSON"
+    },
+    {
+      title: "A function inside a method is called a(n) ____.",
+      choices: ["method", "argument", "prompt", "sequence"],
+      answer: "method"
+    },
+    {
+      title: "Which term will be used in the formula for a random number?",
+      choices: ["Math.door", "Math.wall", "Math.floor", "Math.ceiling"],
+      answer: "Math.floor"
+    },
   ];
 // Display a landing page with a  welcome message and a button that sends user to the first question
 // Display first question with 4 possible answers:
 var incr = 0;
 var body = document.querySelector('body');
-var isItRight = document.createElement("div");
 
+
+displayQuestion()
 function displayQuestion(){
   
   var questionEl = document.createElement("div");
   questionEl.textContent = questions[incr].title;
   body.appendChild(questionEl);
+  displayMultiChoice();
 }
-displayQuestion()
+
 
 function displayMultiChoice(){
   for(i=0; i < 4; i++){
@@ -37,18 +54,38 @@ function displayMultiChoice(){
 
 // Determine if each question is right or wrong.
 function gradeQuestion (event) {    
-  
+  var isItRight = document.createElement("div");
   if (event.target.dataset.correct == event.target.dataset.choice){
-    isItRight.textContent = "You got it right!"
-          body.appendChild(isItRight)
-        }
-    else{
+      isItRight.textContent = "You got it right!"
+      body.appendChild(isItRight)
+      setTimeout(function()
+      {(incr++);
+      document.body.innerHTML = "";  
+        if(incr > questions.length){
+
+       }
+       else{
+          displayQuestion();
+       }
+
+        }, 2000);}
+  else{
     isItRight.textContent = "You got it wrong!"
     body.appendChild(isItRight)
-    }}};    
+    setTimeout(function(){
+      (incr++);
+      document.body.innerHTML = "";  
+        if(incr > questions.length){
+
+       }
+       else{
+          displayQuestion();
+       }
+
+        }, 2000)}}};
 
 
-displayMultiChoice();
+
 
  
  // If correct, increase the score by 1, if incorrect, no change to score
