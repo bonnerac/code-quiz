85b17ad5933f20464e1a3945512bfb988cacbb7d
bonnerac <alex.c.bonner@gmail.com>
2019-12-19 18:43:42 -0600

    added starting page and scorecard

--- script.js
+++ script.js
@@ -26,22 +26,54 @@ var questions = [
       answer: "Math.floor"
     },
   ];
-// Display a landing page with a  welcome message and a button that sends user to the first question
-// Display first question with 4 possible answers:
+
+var timer  
+var score = 0;
 var incr = 0;
 var body = document.querySelector('body');
 
 
-displayQuestion()
+
+displayLandingPage();
+
+// Display a landing page with a  welcome message and a button that sends user to the first question
+function displayLandingPage(){
+  var title = document.createElement("div");
+  title.textContent = "Code Quiz"
+  body.appendChild(title);
+  var welcomeText = document.createElement("div");
+  welcomeText.textContent = "Welcome to the Code Quiz! You will have " + (questions.length)*15 + " seconds to complete " + (questions.length) + " questions. Incorrect answers will deduct 5 seconds from your overall time. Good Luck!"
+  body.appendChild(welcomeText);
+  var startButton = document.createElement("button");
+  startButton.textContent = "Click Here To Start The Quiz"
+  body.appendChild(startButton);
+  var scoreBox = document.createElement("div");
+  scoreBox.textContent = "Score: " + score;
+  body.appendChild(scoreBox);
+  
+  startButton.addEventListener("click", function(){
+    document.body.innerHTML = "";
+    displayQuestion(); 
+  })
+}
+// Display first question 
 function displayQuestion(){
   
   var questionEl = document.createElement("div");
   questionEl.textContent = questions[incr].title;
   body.appendChild(questionEl);
-  displayMultiChoice();
+  displayScore();
+  
 }
+// Display the score
+function displayScore(){
+var scoreBox = document.createElement("div");
+  scoreBox.textContent = "Score: " + score;
+  body.appendChild(scoreBox);
+  displayMultiChoice();
+};
 
-
+// Display 4 possible answers on buttons
 function displayMultiChoice(){
   for(i=0; i < 4; i++){
     var button = document.createElement("button");
@@ -50,39 +82,48 @@ function displayMultiChoice(){
     button.setAttribute('data-choice', questions[incr].choices[i]);
     button.onclick = gradeQuestion
     body.appendChild(button);
-  }
+    
+  }};
 
-// Determine if each question is right or wrong.
+// Determine if each question is right or wrong. If correct, send correct message, increase score by 1 and go on to next question. If incorrect, send incorrect message, and go on to next question. If last question, got on to finalScore
 function gradeQuestion (event) {    
   var isItRight = document.createElement("div");
   if (event.target.dataset.correct == event.target.dataset.choice){
       isItRight.textContent = "You got it right!"
       body.appendChild(isItRight)
+      score ++;
       setTimeout(function()
       {(incr++);
       document.body.innerHTML = "";  
-        if(incr > questions.length){
-
+        if(incr >= questions.length){
+          finalScore();
        }
        else{
           displayQuestion();
        }
-
-        }, 2000);}
+        }, 1500);}
   else{
     isItRight.textContent = "You got it wrong!"
     body.appendChild(isItRight)
     setTimeout(function(){
       (incr++);
       document.body.innerHTML = "";  
-        if(incr > questions.length){
-
+        if(incr >= questions.length){
+          finalScore();
        }
        else{
           displayQuestion();
        }
+        }, 1500)}
+      };
 
-        }, 2000)}}};
+
+// Display final score leaderboard
+
+function finalScore(){
+ 
+
+}
 
 
 
